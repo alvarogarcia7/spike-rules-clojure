3d459eba9baf7aa6c9f8b8f5e2be11b7dae88f35
Alvaro Garcia <derecerca@gmail.com>
2015-10-24 00:48:25 +0200

    add checking for rules :empty and :not-empty

---
+++ test/spikerules/test.clj
@@ -0,0 +1,24 @@
+(ns spikerules.test
+  (:use midje.sweet))
+
+(def rules-repository
+  {
+   :not-empty (fn [value] (not (empty? value)))
+   :empty (fn [value] (empty? value))
+   })
+
+(defn is-valid? [rules]
+  (let [current-rule (:url rules)
+        rules2 (:is current-rule)]
+    (cond
+      (empty? rules2) true
+      :else (((first rules2) rules-repository) (:value current-rule)))))
+
+(fact "check rules' validity"
+      (is-valid? {:url {:value "a"}}) => true
+      (is-valid? {:url {:value "a" :is [:not-empty]}}) => true
+      (is-valid? {:url {:value "" :is [:not-empty]}}) => false
+      (is-valid? {:url {:value "a" :is [:empty]}}) => false
+      (is-valid? {:url {:value "" :is [:empty]}}) => true
+      )
+
