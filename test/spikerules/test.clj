0e2d05c62efc51bc627807678c6422688e6eeaed
Alvaro Garcia <derecerca@gmail.com>
2015-10-24 01:29:30 +0200

    red: apply two rules to the same data

--- test/spikerules/test.clj
+++ test/spikerules/test.clj
@@ -1,11 +1,17 @@
 (ns spikerules.test
   (:use midje.sweet))
 
+;http://stackoverflow.com/questions/5621279/in-clojure-how-can-i-convert-a-string-to-a-number
+(defn- string->integer [s]
+  (when-let [d (re-find #"-?\d+" s)] (Integer. d)))
+
+
 (def rules-repository
   {:not-empty (comp not empty?)
    :empty     empty?
    :single-space (partial = " ")
-   })
+   :number       (fn [number] (not (nil? (string->integer number))))}
+  )
 
 (defn is-valid? [rules]
   (let [current-rule (:url rules)
@@ -19,8 +25,8 @@
       ((:not-empty rules-repository) "") => false
       ((:empty rules-repository) "") => true
       ((:empty rules-repository) "1") => false
-      ((:single-space rules-repository) "") => false
-
+      ((:number rules-repository) " ") => false
+      ((:number rules-repository) "2") => true
       )
 
 (fact "check rules' validity"
@@ -28,6 +34,7 @@
       (is-valid? {:url {:value "a" :is [:not-empty]}}) => true
       (is-valid? {:url {:value "" :is [:not-empty]}}) => false
       (is-valid? {:url {:value "a" :is [:empty]}}) => false
-      (is-valid? {:url {:value "" :is [:empty]}}) => true
+      (is-valid? {:url {:value " " :is [:not-empty :single-space]}}) => true
+      (is-valid? {:url {:value "2" :is [:number :single-space]}}) => false
       )
 
